75b0e2e6186c25370d196ae1f863d731463fe31d
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-22 13:03:18 +0200

    Hockeyplayers from file

--- Cms20HockeySystem/Program.cs
+++ Cms20HockeySystem/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
 
 namespace Cms20HockeySystem
 {
@@ -6,7 +9,36 @@ namespace Cms20HockeySystem
     {
         static void Main(string[] args)
         {
+            var allPlayers = ReadPlayersFromFile();
+            foreach (var p in allPlayers)
+            {
+                Console.WriteLine($"{p.Namn} {p.JerseyNumber}");
+            }
             Console.WriteLine("Hello World!");
+        }
+
+        private static List<Player> ReadPlayersFromFile()
+        {
+            var players = new List<Player>();
+
+            using (var sr =
+                File.OpenText(@"C:\Users\stefan\source\repos\Cms20HockeySystem\Cms20HockeySystem\HockeyPlayers.txt"))
+            {
+                while (true)
+                {
+                    var line = sr.ReadLine();
+                    if (line == null) break;
+                    //Mats Sundin,13
+                    string[] split = line.Split(',');
+                    var p = new Player();
+                    p.Namn = split[0];
+                    p.JerseyNumber = Convert.ToInt32(split[1]);
+                    players.Add(p);
+                }
+
+            }
+
+            return players;
         }
     }
 }

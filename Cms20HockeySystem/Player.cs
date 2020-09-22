c505402933fd07f19f0c0ac7198ba8fbff99e263
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-09-22 12:38:54 +0200

    Add project files.

---
+++ Cms20HockeySystem/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace Cms20HockeySystem
+{
+    public class Player
+    {
+        public string Namn { get; set; }
+        public int JerseyNumber { get; set; }
+    }
+}
---
+++ Cms20HockeySystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cms20HockeySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
